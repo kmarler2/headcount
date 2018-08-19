1042bf0e97d89dcb1c4ba3e4587ddc7c48c5f68c
Keeliana Marler <kmmarler2@hotmail.com>
2018-08-18 18:35:01 -0600

    Setup CardContainer tests

--- src/CardContainer/cardContainer.test.js
+++ src/CardContainer/cardContainer.test.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { shallow } from 'enzyme';
+import DistrictRepository from './helper.js'
+import data from './data/kindergartners_in_full_day_program.js';
 
+describe('app', () => { 
+  let renderedApp;
+  let mockData;
+  let district = new DistrictRepository(data);
 
+  beforeEach(() => {
+    mockData = {
+             '2004': 0.24,
+              '2005': 0.278,
+              '2006': 0.337,
+              '2007': 0.395,
+              '2008': 0.536,
+              '2009': 0.598,
+              '2010': 0.64,
+              '2011': 0.672,
+              '2012': 0.695,
+              '2013': 0.703,
+              '2014': 0.741 }
+
+    renderedApp = shallow(renderedApp=(<App />));
+  });
+})
