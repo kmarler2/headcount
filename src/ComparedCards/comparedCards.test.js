87f259847d40e0a7e3c7114f8e1e42aaf2936dad
Keeliana Marler <kmmarler2@hotmail.com>
2018-08-19 21:05:14 -0600

    Final eslint run

--- src/ComparedCards/comparedCards.test.js
+++ src/ComparedCards/comparedCards.test.js
@@ -9,52 +9,17 @@ describe('DisplayComparedCards component', () => {
   const mockDistrict = new DistrictRepository(data);
   const mockFDBC = jest.fn();
   const mockCDA = jest.fn();
-  const mockCards = [{
-    location: "West Yuma",
-    selected: true,
-    stats: {
-      2004: 0.101, 
-      2005: 0.101,
-      2006: 0.101,
-      2007: 0.101,
-      2008: 0.101,
-      2009: 0.101,
-      2010: 0.101,
-      2011: 0.101,
-      2012: 0.101,
-      2013: 0.101,
-      2014: 0.101
-    }
-  },
-  {
-    location: "West Yuma",
-    selected: true,
-    stats: {
-      2004: 0.101,
-      2005: 0.101,
-      2006: 0.101,
-      2007: 0.101,
-      2008: 0.101,
-      2009: 0.101,
-      2010: 0.101,
-      2011: 0.101,
-      2012: 0.101,
-      2013: 0.101,
-      2014: 0.101
-    }
-  }];
-
   beforeEach(() => {
     wrapper = shallow(<DisplayComparedCards 
-                        data={mockDistrict}
-                        findDistrictByClick={mockFDBC}
-                        compareDistrictAverages={mockCDA} 
-                      />);
+      data={mockDistrict}
+      findDistrictByClick={mockFDBC}
+      compareDistrictAverages={mockCDA} 
+    />);
   });
 
   it('should be defined', () => {
     expect(wrapper).toBeDefined();
-  })
+  });
 
   it('should match its snapshot', () => {
     expect(wrapper).toMatchSnapshot();
--- src/Search/index.js
+++ src/Search/index.js
@@ -16,7 +16,6 @@ class Search extends Component {
           placeholder='Search for a district'
           type="text"
           onChange={ this.handleChange } />
-        <button className='button'></button> 
       </form>
     );
   }
